b6f28e5d05c70859c6f37f211660dcf9453dca2f
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-01-22 19:18:15 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,59 @@
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
-// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-// <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>
-
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
-
 // Закриття з клавіатури
 // Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
 
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryDiv.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryDiv.addEventListener("click", hendlerGetLargeImg);
+
+// ------functions----------
+
+function createGalleryMarkup(elements) {
+  return elements
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+function hendlerGetLargeImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const largeImg = basicLightbox.create(
+    `<img
+      src="${e.target.dataset.source}"
+      width="800" 
+      height="600"
+    />`,
+
+    {
+      onShow: (largeImg) => {
+        document.onkeydown = function (e) {
+          e.preventDefault();
+          if (e.code == "Escape") {
+            largeImg.close();
+          }
+        };
+      },
+    }
+  );
+
+  largeImg.show();
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryDiv = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup2(galleryItems);
 
-console.log(galleryItems);
+function createGalleryMarkup2(elements) {
+  return elements
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
