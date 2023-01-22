473f1e6a36efd6efd42cfd1736862b04e1b89bcf
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-01-22 22:04:27 +0200

    task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,22 +1,28 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryDiv = document.querySelector(".gallery");
-const galleryMarkup = createGalleryMarkup2(galleryItems);
+const galleryUl = document.querySelector(".gallery");
+const galleryMarkup2 = createGalleryMarkup2(galleryItems);
+galleryUl.insertAdjacentHTML("beforeend", galleryMarkup2);
+var lightbox = new SimpleLightbox(".gallery a", {
+  href: "${original}",
+  src: "${preview}",
+  alt: "${description}",
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
+// ------functions----------
 function createGalleryMarkup2(elements) {
   return elements
     .map(
       ({ preview, original, description }) =>
-        `<div class="gallery__item">
-   <a class="gallery__link" href="${original}">
-     <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
     )
     .join("");
 }
